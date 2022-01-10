dfb4db274916826232b5d50ccf7701c54c6558ca
JasperBraun <jasperbraun90@gmail.com>
2022-01-10 12:08:07 -0500

    Fix issue #14

--- tests/utilities_test.py
+++ tests/utilities_test.py
@@ -4,7 +4,7 @@ from copy import deepcopy
 from numpy import allclose, array, inf
 from pytest import mark
 
-from metacommunity.utilities import power_mean, unique_correspondence
+from diversity.utilities import power_mean, unique_correspondence
 
 
 POWER_MEAN_TEST_CASES = [
