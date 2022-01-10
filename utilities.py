260a9b85120fe8abc5334ba1eac188417d6fc699
JasperBraun <jasperbraun90@gmail.com>
2022-01-10 07:53:38 -0500

    Implemented unittest for utilities.unique_correspondence.

--- utilities.py
+++ utilities.py
@@ -4,6 +4,9 @@ Functions
 ---------
 power_mean
     Calculates weighted power means.
+unique_correspondence
+    Describes correspondence between a sequence of items and the set of
+    their unique elements.
 
 Exceptions
 ----------
@@ -22,6 +25,7 @@ from numpy import (
     multiply,
     inf,
     power,
+    unique,
 )
 
 
@@ -35,37 +39,6 @@ class InvalidArgumentError(MetacommunityError):
     """Raised when a function receives an invalid argument."""
 
     pass
-
-
-def unique_correspondence(items, ordered_unique_items=None):
-    """Returns uniqued items and a mapping from items to uniqued items.
-
-    Parameters
-    ----------
-    items: numpy.ndarray
-        Array of items to unique and/or obtain mapping for.
-    ordered_unique_items: Iterable
-        Unique items in desired order. If None, ordering will be
-        established according to numpy.unique.
-
-    Returns
-    -------
-    A tuple with coordinates:
-    0 - numpy.ndarray
-        The ordered unique items.
-    1 - numpy.ndarray
-        The position in the unique items iterable for each item in
-        items.
-    """
-    if ordered_unique_items is None:
-        ordered_unique_items_, item_positions = unique(items, return_inverse=True)
-    else:
-        ordered_unique_items_ = array(ordered_unique_items)
-        item_to_position = {item: pos for pos, item in enumerate(ordered_unique_items_)}
-        if len(item_to_position) != len(ordered_unique_items):
-            raise InvalidArgumentError(f"Expected ordered_unique_items to be uniqued.")
-        item_positions = array([item_to_position[item] for item in items])
-    return (ordered_unique_items_, item_positions)
 
 
 def power_mean(order, weights, items):
@@ -102,3 +75,34 @@ def power_mean(order, weights, items):
     items_product = multiply(items_power, weights, where=mask)
     items_sum = numpy_sum(items_product, axis=0, where=mask)
     return power(items_sum, 1 / order)
+
+
+def unique_correspondence(items, ordered_unique_items=None):
+    """Returns uniqued items and a mapping from items to uniqued items.
+
+    Parameters
+    ----------
+    items: numpy.ndarray
+        Array of items to unique and/or obtain mapping for.
+    ordered_unique_items: Iterable
+        Unique items in desired order. If None, ordering will be
+        established according to numpy.unique.
+
+    Returns
+    -------
+    A tuple with coordinates:
+    0 - numpy.ndarray
+        The ordered unique items.
+    1 - numpy.ndarray
+        The position in the unique items iterable for each item in
+        items.
+    """
+    if ordered_unique_items is None:
+        ordered_unique_items_, item_positions = unique(items, return_inverse=True)
+    else:
+        ordered_unique_items_ = array(ordered_unique_items)
+        item_to_position = {item: pos for pos, item in enumerate(ordered_unique_items_)}
+        if len(item_to_position) != len(ordered_unique_items):
+            raise InvalidArgumentError(f"Expected ordered_unique_items to be uniqued.")
+        item_positions = array([item_to_position[item] for item in items])
+    return (ordered_unique_items_, item_positions)
