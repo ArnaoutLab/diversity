19cb0ea5f38e5de6b53c364707e6affe3c0a329c
Elliot Hill <elliot.douglas.hill@gmail.com>
2023-01-23 00:10:54 -0500

    Refactor components to dictionary for simplicity

--- src/diversity/components.py
+++ src/diversity/components.py
@@ -1,84 +1,82 @@
-from abc import ABC, abstractmethod
 from functools import cached_property
-from typing import Union
-
 from numpy import ndarray
 
 from diversity.abundance import Abundance
 from diversity.similarity import Similarity
 
 
-class Components(ABC):
+class Components:
     """Dispatches diversity components based on specified measure"""
 
-    @abstractmethod
-    def get_numerator(measure: str):
-        pass
-
-    @abstractmethod
-    def get_denominator(measure: str):
-        pass
+    def __init__(self, abundance) -> None:
+        self.abundance = abundance
 
 
 class FrequencySensitiveComponents(Components):
-    """Dispatches frequency-sensitive diversity components based on specified measure"""
+    """Dispatches frequency-sensitive diversity components based on
+    specified measure"""
 
     def __init__(self, abundance: Abundance) -> None:
-        self.abundance = abundance
-
-    def get_numerator(self, measure: str) -> Union[int, ndarray]:
-        if measure in {"alpha", "gamma", "normalized_alpha"}:
-            return 1
-        elif measure in {"beta", "rho", "normalized_beta", "normalized_rho"}:
-            return self.abundance.metacommunity_abundance
-
-    def get_denominator(self, measure: str) -> ndarray:
-        if measure in {"alpha", "beta", "rho"}:
-            return self.abundance.subcommunity_abundance
-        elif measure in {"normalized_alpha", "normalized_beta", "normalized_rho"}:
-            return self.abundance.normalized_subcommunity_abundance
-        elif measure == "gamma":
-            return self.abundance.metacommunity_abundance
+        super().__init__(abundance=abundance)
+        self.numerators = {
+            **dict.fromkeys(["alpha", "gamma", "normalized_alpha"], 1),
+            **dict.fromkeys(
+                ["beta", "rho", "normalized_beta", "normalized_rho"],
+                self.abundance.metacommunity_abundance,
+            ),
+        }
+        self.denominators = {
+            **dict.fromkeys(
+                ["alpha", "beta", "rho"], self.abundance.subcommunity_abundance
+            ),
+            **dict.fromkeys(
+                ["normalized_alpha", "normalized_beta", "normalized_rho"],
+                self.abundance.normalized_subcommunity_abundance,
+            ),
+            "gamma": self.abundance.metacommunity_abundance,
+        }
 
 
 class SimilaritySensitiveComponents(Components):
-    """Dispatches similarity-sensitive diversity components based on specified measure"""
+    """Dispatches similarity-sensitive diversity components based on
+    specified measure"""
 
     def __init__(self, abundance: Abundance, similarity: Similarity) -> None:
-        self.abundance = abundance
+        super().__init__(abundance=abundance)
         self.similarity = similarity
+        self.numerators = {
+            **dict.fromkeys(["alpha", "gamma", "normalized_alpha"], 1),
+            **dict.fromkeys(
+                ["beta", "rho", "normalized_beta", "normalized_rho"],
+                self.metacommunity_similarity,
+            ),
+        }
+        self.denominators = {
+            **dict.fromkeys(["alpha", "beta", "rho"], self.subcommunity_similarity),
+            **dict.fromkeys(
+                ["normalized_alpha", "normalized_beta", "normalized_rho"],
+                self.normalized_subcommunity_similarity,
+            ),
+            "gamma": self.metacommunity_similarity,
+        }
 
     @cached_property
     def metacommunity_similarity(self) -> ndarray:
         return self.similarity.weighted_similarities(
-            self.abundance.metacommunity_abundance
+            relative_abundance=self.abundance.metacommunity_abundance
         )
 
     @cached_property
     def subcommunity_similarity(self) -> ndarray:
         return self.similarity.weighted_similarities(
-            self.abundance.subcommunity_abundance
+            relative_abundance=self.abundance.subcommunity_abundance
         )
 
     @cached_property
     def normalized_subcommunity_similarity(self) -> ndarray:
         return self.similarity.weighted_similarities(
-            self.abundance.normalized_subcommunity_abundance
+            relative_abundance=self.abundance.normalized_subcommunity_abundance
         )
-
-    def get_numerator(self, measure: str) -> Union[int, ndarray]:
-        if measure in {"alpha", "gamma", "normalized_alpha"}:
-            return 1
-        elif measure in {"beta", "rho", "normalized_beta", "normalized_rho"}:
-            return self.metacommunity_similarity
-
-    def get_denominator(self, measure: str) -> ndarray:
-        if measure in {"alpha", "beta", "rho"}:
-            return self.subcommunity_similarity
-        elif measure in {"normalized_alpha", "normalized_beta", "normalized_rho"}:
-            return self.normalized_subcommunity_similarity
-        elif measure == "gamma":
-            return self.metacommunity_similarity
 
 
 def make_components(
