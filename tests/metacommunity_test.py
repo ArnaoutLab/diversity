20b41a22b402d19bc87d4508a1fa39399f8a8d2f
Phuc Nguyen <33138579+nhongphuc@users.noreply.github.com>
2023-12-08 16:18:17 -0500

    removed some irrelevant paths

--- tests/metacommunity_test.py
+++ tests/metacommunity_test.py
@@ -1,14 +1,3 @@
-import sys
-sys.path = ['/Users/arnaoutlab/Desktop/diversity_package-main/src',
- '/Users/arnaoutlab/Desktop/diversity_package-main',
- '/Users/arnaoutlab/Desktop/~/Documents/GitHub/vader',
- '/Users/arnaoutlab/.pyenv/versions/3.9.16/lib/python39.zip',
- '/Users/arnaoutlab/.pyenv/versions/3.9.16/lib/python3.9',
- '/Users/arnaoutlab/.pyenv/versions/3.9.16/lib/python3.9/lib-dynload',
- '',
- '/Users/arnaoutlab/.local/lib/python3.9/site-packages',
- '/Users/arnaoutlab/.pyenv/versions/3.9.16/lib/python3.9/site-packages']
-
 """Tests for diversity.metacommunity."""
 
 from dataclasses import dataclass, field
